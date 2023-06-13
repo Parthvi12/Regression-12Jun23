c524af9f697e426e63e7d38e5479a5744edbccaa
kavita.s <kavita.s@moolya.com>
2023-06-13 06:59:32 +0000

    m

---
+++ pages/icon widget testing/jsobjects/util/util.js
@@ -0,0 +1,21 @@
+export default {
+	myVar1: [],
+	Await :async () => {
+
+           
+    roles: [
+      {
+        label: 'Admin',
+        value: 'ADMIN'
+      },
+      {
+        label: 'Developer',
+        value: 'DEV'
+      },
+      {
+        label: 'Viewer',
+        value: 'VIEWER'
+      }
+    ]
+}
+}
